843555f1a46a8cd10269ead379d29209ec144551
hamed emad <hamadaemadeldine@gmail.com>
2020-06-06 00:26:33 +0200

    Updates

---
+++ precache-manifest.aff157fff3bc1d91daf6398a862fa6aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8555a45d52436e185381e41df3d4e514",
+    "url": "/Pokedex-App/index.html"
+  },
+  {
+    "revision": "8e65c3e077549c64dc4d",
+    "url": "/Pokedex-App/static/css/main.a9bb4ebc.chunk.css"
+  },
+  {
+    "revision": "f730cb180c00ea4af5ba",
+    "url": "/Pokedex-App/static/js/2.5ef191f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pokedex-App/static/js/2.5ef191f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e65c3e077549c64dc4d",
+    "url": "/Pokedex-App/static/js/main.7c7721ee.chunk.js"
+  },
+  {
+    "revision": "9ee7e1b9ed48dbaded88",
+    "url": "/Pokedex-App/static/js/runtime-main.185346a1.js"
+  }
+]);
